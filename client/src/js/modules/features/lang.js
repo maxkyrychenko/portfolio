a0bd6b3639013939e38449f81a52530a70564c01
Max Kemzi <kemzi9585@gmail.com>
2023-01-20 19:06:19 +0200

    fix: fix language issue

--- client/src/js/modules/features/lang.js
+++ client/src/js/modules/features/lang.js
@@ -1,52 +1,50 @@
-window.onload = () => {
-	const langButtons = document.querySelectorAll("[data-type='lang-btn']");
+if (!location.href.includes("#")) {
+	location.href = window.location.pathname + "#en";
+}
 
-	if (!location.href.includes("#")) {
-		location.href = window.location.pathname + "#en";
-	}
+const langButtons = document.querySelectorAll("[data-type='lang-btn']");
 
-	langButtons.forEach(button => {
-		button.addEventListener("click", () => {
-			location.hash = "#" + button.textContent.toLowerCase();
-			location.reload();
-		});
+langButtons.forEach(button => {
+	button.addEventListener("click", () => {
+		location.hash = "#" + button.textContent.toLowerCase();
+		location.reload();
+	});
+});
+
+const updateElements = hash => {
+	const elements = document.querySelectorAll(`[data-${hash}Lang]`);
+
+	elements.forEach(elem => {
+		if (
+			(elem.tagName.toLowerCase() === "input" ||
+				elem.tagName.toLowerCase() === "textarea") &&
+			elem.hasAttribute("placeholder")
+		) {
+			elem.setAttribute("placeholder", elem.getAttribute(`data-${hash}Lang`));
+		} else {
+			elem.childNodes[0].nodeValue = elem.getAttribute(`data-${hash}Lang`);
+		}
+	});
+};
+
+const changeLanguage = () => {
+	const projectsList =
+		document.querySelector(".projects-filter__items") ||
+		document.querySelector(".projects-slider__wrapper");
+	const hash = window.location.hash.substring(1);
+
+	langButtons.forEach(btn => {
+		if (btn.textContent.toLowerCase() === hash) {
+			btn.classList.add("active");
+			btn.disabled = true;
+		}
 	});
 
-	const updateElements = hash => {
-		const elements = document.querySelectorAll(`[data-${hash}Lang]`);
+	updateElements(hash);
 
-		elements.forEach(elem => {
-			if (
-				(elem.tagName.toLowerCase() === "input" ||
-					elem.tagName.toLowerCase() === "textarea") &&
-				elem.hasAttribute("placeholder")
-			) {
-				elem.setAttribute("placeholder", elem.getAttribute(`data-${hash}Lang`));
-			} else {
-				elem.childNodes[0].nodeValue = elem.getAttribute(`data-${hash}Lang`);
-			}
-		});
-	};
+	const observer = new MutationObserver(() => updateElements(hash));
 
-	const changeLanguage = () => {
-		const projectsList =
-			document.querySelector(".projects-filter__items") ||
-			document.querySelector(".projects-slider__wrapper");
-		const hash = window.location.hash.substring(1);
-
-		langButtons.forEach(btn => {
-			if (btn.textContent.toLowerCase() === hash) {
-				btn.classList.add("active");
-				btn.disabled = true;
-			}
-		});
-
-		updateElements(hash);
-
-		const observer = new MutationObserver(() => updateElements(hash));
-
-		observer.observe(projectsList, {childList: true});
-	};
-
-	changeLanguage();
+	observer.observe(projectsList, {childList: true});
 };
+
+changeLanguage();
